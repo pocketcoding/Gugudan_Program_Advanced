fd7e6149499b9f2a4df429297d62f73ebadae63b
mac <mac@macui-MacBookAir.local>
2020-05-04 23:23:48 +0900

    Getting the user input(String), and split the value into two before changing the value type into Integer. Printing out the (ex) 8*7 times table

--- src/code/Gugudan.java
+++ src/code/Gugudan.java
@@ -1,9 +1,29 @@
 package code;
 
+import java.util.Scanner;
+
 public class Gugudan {
 	
 	public static void main(String[] args)
 	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("2 numbers for a * b times table : ");
+		String inputValue = scanner.nextLine();
+		
+		String[]  splitedValue = inputValue.split(",");
+		
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		for(int i = 2; i <= first; i++)
+		{
+			for(int j = 1; j <= second; j++)
+			{
+				System.out.println(i + " * " + j + " = " + i * j);
+			}
+			
+		}
+		
 		
 	}
 
